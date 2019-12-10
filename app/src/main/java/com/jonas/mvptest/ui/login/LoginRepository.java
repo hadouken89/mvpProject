4396d6364b38a721434531b35d19b3999b94242d
hadouken89 <jonas_88mig@hotmail.com>
2019-12-10 00:17:19 -0300

    add LoginRepository and MemoryRepository

---
+++ app/src/main/java/com/jonas/mvptest/ui/login/LoginRepository.java
@@ -0,0 +1,10 @@
+package com.jonas.mvptest.ui.login;
+
+import com.jonas.mvptest.model.User;
+
+public interface LoginRepository {
+
+    void saveUser();
+
+    User getUser();
+}
---
+++ app/src/main/java/com/jonas/mvptest/ui/login/MemoryRepository.java
@@ -0,0 +1,16 @@
+package com.jonas.mvptest.ui.login;
+
+import com.jonas.mvptest.model.User;
+
+public class MemoryRepository implements LoginRepository {
+
+    @Override
+    public void saveUser() {
+
+    }
+
+    @Override
+    public User getUser() {
+        return null;
+    }
+}
